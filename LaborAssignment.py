e80bf5c746e3de65e06a1a6035fba8aef5f3adb0
Erick Cardenas <ecardenas@utexas.edu>
2015-08-06 13:46:41 -0700

    LaborAssignment.py prints min_st_edge_cut now
    min_st_edge_cut would represent the labor assignments

--- LaborAssignment.py
+++ LaborAssignment.py
@@ -9,13 +9,15 @@ class Edge(object):
 	def __repr__(self):
 		return "%s->%s:%s" % (self.source, self.sink, self.capacity)
 
-class FlowNetwork(object):
+class Graph(object):
 	def __init__(self):
 		self.adj = {}
 		self.flow = {}
+		self.vertex_set = set()
 
 	def add_vertex(self, vertex):
 		self.adj[vertex] = []
+		self.vertex_set.add(vertex)
 
 	def get_edges(self, v):
 		return self.adj[v]
@@ -43,12 +45,45 @@ class FlowNetwork(object):
 				if result != None:
 					return result
 
-	def max_flow(self, source, sink):
-		path = self.find_path(source, sink, [], set())
-		while path != None:
-			flow = min(res for edge,res in path)
-			for edge,res in path:
-				self.flow[edge] += flow
-				self.flow[edge.redge] -= flow
-			path = self.find_path(source, sink, [], set())
-		return sum(self.flow[edge] for edge in self.get_edges(source))
+def dfs(G, s, visited):
+    visited[s] = True;
+    for edge in G.get_edges(s):
+    	residual = edge.capacity - G.flow[edge]
+    	if (residual > 0 and not visited[edge.sink]):
+           dfs(G, edge.sink, visited)
+
+
+def min_st_edge_cut(G, s, t):
+	path = G.find_path(s, t, [], set())
+	while path != None:
+		flow = min(res for edge,res in path)
+		for edge,res in path:
+			G.flow[edge] += flow
+			G.flow[edge.redge] -= flow
+		path = G.find_path(s, t, [], set())
+	
+	# Flow is maximum now, find vertices reachable from s
+	visited = {}.fromkeys(G.vertex_set, False)
+	dfs(G, s, visited)
+
+    # Print all edges that are from a reachable vertex to
+    # non-reachable vertex in the original graph
+	for v in G.vertex_set:
+		for edge in G.get_edges(v):
+			if visited[v] and not visited[edge.sink] and edge.capacity > 0:
+				print v, ' - ', edge.sink
+
+g = Graph()
+map(g.add_vertex, ['0', '1', '2', '3', '4', '5'])
+g.add_edge('0', '1', 16)
+g.add_edge('0', '2', 13)
+g.add_edge('1', '2', 10)
+g.add_edge('1', '3', 12)
+g.add_edge('2', '4', 14)
+g.add_edge('2', '1', 4)
+g.add_edge('3', '2', 9)
+g.add_edge('3', '5', 20)
+g.add_edge('4', '3', 7)
+g.add_edge('4', '5', 4)
+min_st_edge_cut(g, '0', '5')
+
